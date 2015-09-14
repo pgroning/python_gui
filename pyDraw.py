e6b33253544a8cb1f0afa5c7c9cb4171e07dcdb2
Per Gröningsson <per.groningsson@gmail.com>
2015-09-14 21:08:26 +0200

    changed class name in pyDraw.py

--- pyDraw.py
+++ pyDraw.py
@@ -4,10 +4,10 @@
 import sys, random
 from PyQt4 import QtGui, QtCore
 
-class Example(QtGui.QWidget):
+class Bundle(QtGui.QWidget):
     
     def __init__(self):
-        super(Example, self).__init__()
+        super(Bundle, self).__init__()
         
         self.initUI()
         
@@ -211,7 +211,7 @@ class Example(QtGui.QWidget):
 def main():
     
     app = QtGui.QApplication(sys.argv)
-    ex = Example()
+    ex = Bundle()
     sys.exit(app.exec_())
 
 
